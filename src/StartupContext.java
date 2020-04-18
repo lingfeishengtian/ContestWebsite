6e238e6943e03e91c35fa47a458b74958500ff66
lingfeishengtian <hunter.han@gmail.com>
2020-04-18 00:52:31 -0500

    edit StartupContext

--- src/StartupContext.java
+++ src/StartupContext.java
@@ -2,6 +2,9 @@ import utils.data_management.DatabaseUtils;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 public class StartupContext
@@ -9,9 +12,20 @@ public class StartupContext
     @Override
     public void contextInitialized(ServletContextEvent arg0) {
         try {
+            Path folder = Paths.get(arg0.getServletContext().getRealPath("") + "WEB-INF/");
+            createFolderIfNotExist(folder.resolve("appeals").toString());
             DatabaseUtils.setConnection(arg0.getServletContext().getRealPath(""));
+            System.out.println(folder.toString());
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static final boolean createFolderIfNotExist(String fileName){
+        File f = new File(fileName);
+        if(!f.exists()){
+           return f.mkdir();
+        }
+        return true;
     }
 }
